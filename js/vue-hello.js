c7273701f85cf87a52bce660fdebe7b70bf5df32
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-28 16:42:44 +0200

    completato bonus speciale

--- js/vue-hello.js
+++ js/vue-hello.js
@@ -7,5 +7,16 @@ createApp({
             image: 'https://pbs.twimg.com/media/DIYJPGiUMAIKnTu.jpg'
 
         }
+    },
+    methods: {
+        sorpresa: function () {
+            /*return {
+                surprise: 'https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif'
+            }*/
+            alert('sad rick roll :(');
+            return this.sorpresa
+
+        }
     }
+
 }).mount('#app')
